a498aa54c5f54f38f698a536ce140e2e086a95c1
Spencer <spencermstarr@hotmail.com>
2022-09-26 08:33:38 -0500

    Add files via upload

---
+++ Both BE & FS script 2.R
@@ -0,0 +1,180 @@
+# Spencer Marlen-Starr's RStudio script of functions, methods, code, and 
+# code comments required for running Benchmark #2, namely the 
+# Backward Elimination version of Stepwise Regression
+# on some of our random synthetic observations to see how well it 
+# does so we can compare its results with Dr. Davies EER procedure.
+
+
+# find out which working directory R has defaulted to
+getwd()
+setwd("~/DAEN_698/MCS_BMs 2 & 3")
+getwd()
+
+
+### Benchmark #2: 
+### One of the two main versions of the classical method
+### of automated optimal variable selection back before
+### the modern machine learning revolution; namely,
+### the 'Backward Elimination' version of Stepwise Regression.
+
+
+# load all necessary packages
+library(stats)
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(tibble)
+library(leaps)
+library(lars)
+library(stringi)
+library(purrr)
+
+
+# Importing an entire file folder's worth of 1 worksheet csv files, 
+# these worksheets were created using the random observations 
+# generating Macro that Dr. Davies wrote for me with 3 different
+# random possible worksheets created on each individual pass of the 
+# various nested levels  of iteration laid out in the original 
+# draft of the Working Paper's in depth description of the
+# Monte Carlo Simulations used to compare his EER algorithm to
+# the current standard alternatives and those of the past
+# as benchmarks.
+
+
+
+### Step 1 for comparing the results of BE on our
+### randomly generated synthetic sample data,
+### in terms of runtime, accuracy, and probability
+### of accidentally choosing spuriously correlated
+### regressors - Importing & Loading the aforementioned
+### sample observations Dr. Davies
+### generated for this purpose.
+# Import all 531 of the individual csv files, each of which
+# containing 500 rows by 30 columns worth of randomly generated 
+# (synthetic) observations below.
+# In order to accomplish this, I will be using the readr library in R.
+
+
+
+# Extract all of the individual spreadsheet containing workbooks
+# in the file folder called 'Run_3_seven) which is filled
+# random synthetic observations to run Lasso, Stepwise, and eventually EER
+# on to compare the results. There are 531 random spreadsheets in this folder
+directory_path <- "~/DAEN_698/other datasets/sample obs (alternate)"
+filepath_list <- list.files(path = directory_path, full.names = TRUE, recursive = TRUE)
+length(filepath_list)
+str(filepath_list)
+
+# reformat the names of each of the csv file formatted datasets
+DS_names_list <- basename(filepath_list)
+DS_names_list <- tools::file_path_sans_ext(DS_names_list)
+head(DS_names_list, n = 4)
+
+
+## This line reads all of the data in each of the csv files 
+## using the name of each store in the list we just created.
+datasets <- lapply(filepath_list, read.csv)
+# Running lines 77 & 78 to create the 'datasets' list took somewhere
+# between 22 & 23 minutes.
+### Step 1 is complete
+
+
+
+
+### Step 3: Run a Backward Elimination Stepwise Regression
+### function on each of the 47,500 datasets.
+### Assign the full models to their corresponding datasets and
+### store these in the object "all_regressors_models"
+set.seed(11)      # for reproducibility
+full_models <- vector("list", length = length(datasets))
+BE_fits <- vector("list", length = length(datasets))
+head(BE_fits)   # returns a list with 18 elements, all of which are NULL
+
+
+set.seed(11)      # for reproducibility
+for(i in seq_along(datasets)) {
+  full_models[[i]] <- lm(formula = Y ~ ., data = datasets[[i]])
+  BE_fits[[i]] <- step(object = full_models[[i]], 
+                        scope = formula(full_models[[i]]),
+                        direction = 'backward',
+                        trace = 0)
+}
+
+head(full_models, n = 2)
+head(BE_fits, n = 2)
+
+
+Variables_selected_by_BE <- lapply(seq_along(BE_fits), 
+                              \(i) names(coef(BE_fits[[i]])))
+
+head(Variables_selected_by_BE, n = 3)
+
+
+### Again, the final format of the results should look
+### like the following:  "#-#-#-#, X#, X#, X#, X#, etc. 
+# Important note! BE can wing up with a different number 
+# of X#s to select.
+## print out the output formatted the way Dr. Davies asked for!
+write.csv(data.frame(DS_name = DS_names_list, 
+                     Variables_selected = sapply(Variables_selected_by_BE, 
+                                                 toString)), 
+          file = "BE2_results.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+### Step 4/5 (optional): Run a Forward Selection Stepwise Regression
+### function on each of the 47,500 datasets.
+### Assign the null models to their corresponding datasets and
+### store these in the object "null_models"
+set.seed(11)      # for reproducibility
+#datasets[[1]]$
+null_model = lm(datasets[[1]]$Y ~ 1, data = datasets[[1]])
+null_model
+null_model[1]
+null_model[[1]]
+
+
+null_models <- vector("list", length = length(datasets))
+null_models[[1]]
+null_models[[2]]
+head(null_model)
+head(null_models, n = 3)
+FS_fits <- vector("list", length = length(datasets))
+head(FS_fits, n = 3)   # returns a list with 15 elements, all of which are NULL
+
+set.seed(11)      # for reproducibility
+for(j in seq_along(datasets)) { null_models[[j]] = lm(formula = Y ~ 1, 
+                                                     data = datasets[[j]]) 
+                                FS_fits[[j]] = step(object = null_models[[j]],
+                                                    direction = 'forward',
+                       scope = formula(full_models[[j]]), trace = 0) }
+
+
+head(null_model, n = 1)
+head(null_models, n = 1)
+head(null_model, n = 4)
+head(null_models, n = 4)
+
+head(FS_fits, n = 1)
+names(coef(FS_fits[[1]]))
+names(coef(FS_fits[[2]]))
+names(coef(FS_fits[[3]]))
+
+
+FS_Coeffs <- lapply(seq_along(FS_fits), function(i) coef(FS_fits[[i]]))
+
+IVs_selected_by_FS <- lapply(seq_along(FS_fits), 
+                             \(i) names(coef(FS_fits[[i]])))
+
+
+write.csv(data.frame(DS_name = DS_names_list, 
+                     Variables_selected = sapply(Variables_selected_by_FS, 
+                                                 toString)), 
+          file = "BE2_results.csv", row.names = FALSE)
---
+++ Elastic Net script.R
@@ -0,0 +1,130 @@
+# This script required the user to hit Run either by the button
+# in the top right corner of this panel, or by hitting Ctrl+Enter
+
+getwd()
+setwd("~/DAEN_698/MCS_BM1")
+getwd()
+
+
+
+# load all necessary packages using only 1 command/line
+library_list1 <- c(library(stats),library(dplyr),library(tidyverse),
+                  library(readr),library(stringi))
+
+library_list2 <- c(library(stats), library(leaps),
+                   library(lars), library(elasticnet))
+
+# load all necessary packages 1 by 1 instead
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(stringi)
+library(purrr)
+
+library(stats)
+library(leaps)
+library(lars)
+library(elasticnet)
+
+
+
+# these 2 lines together create a simple character list of 
+# all the file names in the file folder of datasets you created
+directory_path <- "~/DAEN_698/sample obs"
+filepath_list <- list.files(path = directory_path, full.names = TRUE, 
+                        recursive = TRUE)
+length(filepath_list)
+str(filepath_list)
+
+
+
+
+# The code below reads the data into the RStudio Workspace from
+# each of the 47k datasets in an iterative manner in such a way 
+# that it assigns each of them to the corresponding name of that 
+# dataset in the file folder they are stored in.
+datasets <- lapply(list.files(path = "~/DAEN_698/sample obs", 
+                              full.names = TRUE, recursive = TRUE), read.csv)
+
+
+# reformat the names of each of the csv file formatted dataset
+DS_names_list <- basename(filepath_list)
+DS_names_list <- tools::file_path_sans_ext(DS_names_list)
+head(DS_names_list, n = 4)
+str(DS_names_list)
+
+
+
+# This function fits all 47,500 Elastic_Net regressions for/on
+# each of the corresponding 47.5k datasets stored in the object
+# of that name, then outputs standard regression results which 
+# are typically called returned for any regression ran using R
+set.seed(11)     # to ensure replicability
+Elastic_Net_fits <- lapply(datasets, function(i) 
+               enet(x = as.matrix(select(i, starts_with("X"))), 
+               y = i$Y, lambda = 0, normalize = FALSE))
+
+length(Elastic_Net_fits)
+head(str(Elastic_Net_fits), n = 1)
+head(Elastic_Net_fits, n = 1)
+head(Elastic_Net_fits, n = 3)
+summary(Elastic_Net_fits)
+coefficients(Elastic_Net_fits[1])
+
+
+# This stores and prints out all of the regression 
+# equation specifications selected by Elastic_Net when called
+set.seed(11)     # to ensure replicability
+Elastic_Net_Coeffs <- lapply(Elastic_Net_fits, 
+                       function(i) predict(i, x = as.matrix(select(i, starts_with("X"))), 
+                                           s = 0.1, mode = "fraction", 
+                                           type = "intercept")[["intercept"]])
+set.seed(11)     # to ensure replicability
+Elastic_Net_Coeffs <- lapply(Elastic_Net_fits, 
+                       function(i) predict(i, x = as.matrix(select(i, starts_with("X"))), 
+                                           s = 0.1, mode = "fraction", 
+                                           type = "coefficients")[["coefficients"]])
+
+Elastic_Net_Coeffs[[1]]
+all_Coeffs_estimated_by_1st_Elastic_Net <- Elastic_Net_Coeffs[[1]]
+all_Coeffs_estimated_by_1st_Elastic_Net
+Elastic_Net_Coeffs[[1]][["X1"]]
+Elastic_Net_Coeffs[[1]][["intercept"]]
+Elastic_Net_Coeffs[[1]][["Intercept"]]
+
+### Write my own custom function which will separate out and return a 
+### new list containing just the Independent Variables/Factors/Predictors
+### which are 'selected' or chosen for each individual dataset (all 
+### of which contain 15,000 randomly generated synthetic observations)
+### by the unique Elastic_Net Regression ran on it, i.e. the simple linear
+### regression specification which Elastic_Net can find that best fits the 
+### the synthetic observations within that dataset.
+positive_coeffs <- lapply(Elastic_Net_Coeffs, function(i) i[i > 0])
+positive_coeffs[1]
+positive_coeffs[[1]]
+
+
+IVs_selected_by_Elastic_Net <- lapply(Elastic_Net_Coeffs, function(i) names(i[i > 0]))
+IVs_selected_by_Elastic_Net[[1]]
+head(IVs_selected_by_Elastic_Net, n = 4)
+class(IVs_selected_by_Elastic_Net)
+str = str(IVs_selected_by_Elastic_Net)
+str(IVs_selected_by_Elastic_Net[1])
+str(IVs_selected_by_Elastic_Net[2])
+rownames(IVs_selected_by_Elastic_Net)    # output: NULL
+colnames(IVs_selected_by_Elastic_Net)    # output: NULL
+
+
+## Write all the Factors/Predictors the 
+## 47,500 Elastic_Net Regressions to a text file.
+getwd()
+setwd("~/DAEN_698/MCS_BM1")
+setwd("~/DAEN_698")
+getwd()
+
+write.csv(data.frame(DS_name = DS_names_list, 
+                     IVs_selected = sapply(IVs_selected_by_Elastic_Net, 
+                                           toString)), 
+          file = "Benchmark1_results.csv", row.names = FALSE)
+
+
