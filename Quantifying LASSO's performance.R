874bbc75613492cde79d8cbedf00548760c66e08
Spencer <spencermstarr@hotmail.com>
2022-10-06 21:50:01 -0500

    Add files via upload

--- Quantifying LASSO's performance.R
+++ Quantifying LASSO's performance.R
@@ -12,7 +12,83 @@ tail(BM1_models, n = 4)
 str(BM1_models)
 
 
-correct_models <- count(BM1_models, )
+n_df2 <- do.call(
+  rbind.data.frame,
+  lapply(
+    strsplit(BM1_models$V1, ";"),
+    function(x) {
+      s <- strsplit(x, "-")
+      c(s[[1]], s[[2]])
+    } 
+  )
+) |> setNames(
+  c("n1", "n2", "n3", "n4", "IV")
+)
+
+
+sub_3_df <- subset(n_df, n2 == "3")
+head(sub_3_df)
+
+count(sub_3_df, sub_3_df$IV == "X1, X2, X3")
+count(sub_3_df, when(sub_3_df$IV == "X1, X2, X3"))
+CS_3_IV_case <- if (sub_3_df$IV[i] == "X1, X2, X3") {
+  j <- i + 1
+  return(j)
+}
+CS_3_IV_case <- if (sub_3_df$IV[12] == "X1, X2, X3") { 1 }
+CS_3_IV_case <- if (sub_3_df$IV[12] == "X1, X2, X3") { return TRUE } else { return FALSE }
+
+setwd("~/DAEN_698/MCS_BM1/txt & csv files")
+lapply(sub_3_df, sum(CS_3_IV_case[i]))
+
+write.csv(sub_3_df, file = "Selections_on_3_IV_datasets.csv", row.names = FALSE)
+
+
+sub_4_df <- subset(n_df, n2 == "4")
+write.csv(sub_4_df, file = "Selections_on_4_IV_datasets.csv", row.names = FALSE)
+
+sub_5_df <- subset(n_df, n2 == "5")
+write.csv(sub_5_df, file = "Selections_on_5_IV_datasets.csv", row.names = FALSE)
+
+sub_6_df <- subset(n_df, n2 == "6")
+write.csv(sub_6_df, file = "Selections_on_6_IV_datasets.csv", row.names = FALSE)
+
+sub_7_df <- subset(n_df, n2 == "7")
+write.csv(sub_7_df, file = "Selections_on_7_IV_datasets.csv", row.names = FALSE)
+
+sub_8_df <- subset(n_df, n2 == "8")
+write.csv(sub_8_df, file = "Selections_on_8_IV_datasets.csv", row.names = FALSE)
+
+sub_9_df <- subset(n_df, n2 == "9")
+write.csv(sub_9_df, file = "Selections_on_9_IV_datasets.csv", row.names = FALSE)
+
+sub_10_df <- subset(n_df, n2 == "10")
+write.csv(sub_10_df, file = "Selections_on_10_IV_datasets.csv", row.names = FALSE)
+
+sub_11_df <- subset(n_df, n2 == "11")
+write.csv(sub_11_df, file = "Selections_on_11_IV_datasets.csv", row.names = FALSE)
+
+sub_12_df <- subset(n_df, n2 == "12")
+write.csv(sub_12_df, file = "Selections_on_12_IV_datasets.csv", row.names = FALSE)
+
+sub_13_df <- subset(n_df, n2 == "13")
+write.csv(sub_13_df, file = "Selections_on_13_IV_datasets.csv", row.names = FALSE)
+
+sub_14_df <- subset(n_df, n2 == "14")
+write.csv(sub_14_df, file = "Selections_on_14_IV_datasets.csv", row.names = FALSE)
+
+sub_15_df <- subset(n_df, n2 == "15")
+write.csv(sub_15_df, file = "Selections_on_15_IV_datasets.csv", row.names = FALSE)
+
+
+
+
+
+
+
+correct_models <- sapply(BM1_models, count(BM1_models, ))
+
+
 
 
 
