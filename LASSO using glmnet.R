f46a537cbd0e0a038f485c48e0322e3cb67015c7
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-01-07 13:28:10 +0100

    Add files via upload

---
+++ LASSO using glmnet.R
@@ -0,0 +1,162 @@
+# This script required the user to hit Run either by the button
+# in the top right corner of this panel, or by hitting Ctrl+Enter
+
+# find out which working directory R has defaulted to
+getwd()
+
+# copy+paste whatever the path is for your file folder with all the 
+# random synthetic csvs you have created inside a set of quotation marks
+# inside of the setwd() function below and then press Run (or hit Ctrl+Enter)
+setwd()
+# Or, click the Session option in the Ribbon, and in the
+# Set Working Directory's drop down list, select Choose Directory
+# in order to set it manually.
+
+# load all necessary packages using only 1 command/line
+library_list <- c(library(plyr),library(dplyr),library(tidyverse),
+                  library(readr),library(tibble),library(stringi),
+                  library(janitor),library(purrr),library(stats),
+                  library(glmnet),library(vroom),library(data.table),
+                  library(parallel),library(microbenchmark))
+                  
+
+
+all_data <- vroom(list.files(pattern = 'csv'), id = 'source_file')
+
+# these 2 lines together create a simple character list of 
+# all the file names in the file folder of datasets you created
+folderpath <- "C:/Users/Spencer/Documents/EER Project/Data/misc. 1000"
+filepaths_list <- list.files(path = folderpath, full.names = TRUE, 
+                             recursive = TRUE)
+filepaths_list
+
+# reformat the names of each of the csv file formatted dataset
+DS_names_list <- basename(filepaths_list)
+DS_names_list <- tools::file_path_sans_ext(DS_names_list)
+DS_names_list
+
+# sort both of the list of file names so that they are in the proper order
+my_order = DS_names_list |> 
+  # split apart the numbers
+  strsplit(split = "-", fixed = TRUE) |>
+  unlist() |> 
+  # convert them to numeric and get them in a data frame
+  as.numeric() |> 
+  matrix(nrow = length(DS_names_list), byrow = TRUE) |>
+  as.data.frame() |>
+  # get the appropriate ordering to sort the data frame
+  do.call(order, args = _)
+my_order
+DS_names_list[my_order]
+DS_names_list = DS_names_list[my_order]
+
+filepaths_list[my_order]
+filepaths_list = filepaths_list[my_order]
+
+
+
+# The code below reads the data into the RStudio Workspace from
+# each of the 40 datasets in an iterative manner in such a way 
+# that it assigns each of them to the corresponding name of that 
+# dataset in the file folder they are stored in.
+CL <- makeCluster(detectCores() - 1L)
+clusterExport(CL, c('filepaths_list'))
+system.time( datasets2 <- lapply(filepaths_list, read.csv) )
+system.time( datasets <- parLapply(CL, filepaths_list, fread))
+stopCluster(CL)
+class(datasets)   # "list"
+str(datasets)
+
+df1 <- read.csv("0-3-1-3.csv", header = FALSE)
+colnames(df1) <- c("Y", "X1", "X2", "X3", "X4", "X5", "X6", "X7", 
+                  "X8", "X9", "X10", "X11", "X12", "X13", "X14", 
+                  "X15", "X16", "X17", "X18", "X19", "X20", "X21", 
+                  "X22", "X23", "X24", "X25", "X26", "X27", "X28", 
+                  "X29", "X30")
+df1_Structural_IVs <- df1[1, -1]
+df2 <- read.csv("0-3-1-23.csv", header = FALSE)
+All_Headers <- df1[3, ]
+IV_headers <- df1[3, -1]
+
+
+# change column names of all the columns in the dataframe 'datasets'
+CL <- makeCluster(detectCores() - 1L)
+clusterExport(CL, c('datasets'))
+system.time(datasets <- lapply(datasets, function(dataset_i) { 
+  colnames(dataset_i) <- c("Y", "X1", "X2", "X3", "X4", "X5", "X6", "X7", 
+                           "X8", "X9", "X10", "X11", "X12", "X13", "X14", 
+                           "X15", "X16", "X17", "X18", "X19", "X20", "X21", 
+                           "X22", "X23", "X24", "X25", "X26", "X27", "X28", 
+                           "X29", "X30")
+  dataset_i }))
+stopCluster(CL)
+
+
+Structural_IVs_chr <- lapply(datasets, function(j) {j[1, -1]})
+Structural_IVs_num <- lapply(Structural_IVs_chr, \(X) {lapply(X, as.numeric)})
+
+True_IVs <- lapply(datasets, function(j) {j[1:3, -1]})
+True_IVs <- lapply(True_IVs, function(j) {j[-2, ]})
+
+True_Regressors <- lapply(Structural_IVs_chr, function(i) {
+  names(i)[i == 1] })
+True_Regressors
+
+datasets <- lapply(datasets, function(i) {i[-1:-3, ]})
+datasets <- lapply(datasets, \(X) { lapply(X, as.numeric) })
+datasets <- lapply(datasets, function(i) { as.data.table(i) })
+datasets <- lapply(datasets, \(X) { round(X, 3) })
+
+
+
+
+# This function fits all 40 LASSO regressions for/on
+# each of the corresponding 40 datasets stored in the object
+# of that name, then outputs standard regression results which 
+# are typically called returned for any regression ran using R
+CL3 <- makeCluster(detectCores() - 1L)
+clusterExport(CL3, c('datasets'))
+set.seed(11)     # to ensure replicability
+system.time(LASSO.fits <- lapply(datasets, function(i) 
+               glmnet(x = as.matrix(select(i, starts_with("X"))), 
+                      y = i$Y, alpha = 0)))
+
+# This stores and prints out all of the regression 
+# equation specifications selected by LASSO when called
+LASSO.Coeffs <- lapply(LASSO.fits, function(x){
+    t(data.matrix(predict(x, s = 0.1, type = "coefficients")))
+  })
+
+LASSO.Coeffs <- lapply(LASSO.fits, coef.glmnet)
+
+LASSO.Coeffs2 <- lapply(LASSO.fits, 
+                        function(i) predict(i, s = 0.1, type = "coefficients"))
+stopCluster(CL3)
+
+
+
+
+IVs_Selected_by_LASSO <- 
+IVs_Selected_by_LASSO
+
+
+
+### Count up how many Variables Selected match  the true 
+### structural equation variables for that dataset in order
+### to measure LASSO's performance.
+Total_Positives <- lapply(True_Regressors, function(i) { length(i) })
+
+system.time(True_Positives <- lapply(seq_along(datasets), \(i)
+                         sum(IVs_Selected_by_LASSO[[i]] %in% 
+                               True_Regressors[[i]]))) 
+
+
+BM1_TPRs = lapply(seq_along(datasets), \(j)
+             j <- (True_Positives[[j]]/Total_Positives[[j]]) )
+
+BM1_FPRs = lapply(seq_along(datasets), \(i)
+                  i <- length(IVs_Selected_by_LASSO[[i]]) - True_Positives[[i]])
+
+# write one or more lines of code which determine whether each selected 
+# model is "Underspecified", "Correctly Specified", or "Overspecified".
+
