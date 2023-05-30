2517f9ca911076413f5c72a37630a088fa9bce7f
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-05-30 16:37:42 +0200

    Add files via upload

--- Stage 2 scripts/glmnet LASSO (Regressions only).R
+++ Stage 2 scripts/glmnet LASSO (Regressions only).R
@@ -1,12 +1,14 @@
 # This script required the user to hit Run either by the button
-# in the top right corner of this panel, or by hitting Ctrl+Enter
+# in the top right corner of this panel, or by hitting Ctrl+Alt+R.
 
 # find out which working directory R has defaulted to
-#rm(list = ls())
+rm(list = ls())
 #setwd("D:/EER")
 #setwd("D:/EER folder")
 #setwd("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER/1st Benchmark")
 #getwd()
+system.time( load("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER Project/Saved WorkSpaces/Workspaces for dataset folders starting with '0.75'/datasets WorkSpace for '0.75-9-1-1 to 0.75-9-10-500'.RData") )
+#Structural_Variables <- True_Regressors
 
 # copy+paste whatever the path is for your file folder with all the 
 # random synthetic csvs you have created inside a set of quotation marks
@@ -20,7 +22,15 @@
 library_list <- c(library(plyr),library(dplyr),library(stringi),
                   library(purrr),library(stats),library(glmnet),
                   library(data.table),library(parallel))
-                  
+
+
+All_Variable_Names <- lapply(datasets, function(k) {names(k)})
+
+# assign all 30 candidate regressor names to an object
+var_names <- c("X1","X2","X3","X4","X5","X6","X7","X8",
+               "X9","X10","X11","X12","X13","X14","X15",
+               "X16","X17","X18","X19","X20","X21","X22", 
+               "X23","X24","X25","X26","X27","X28","X29","X30")                  
 
 
 # This function fits all n LASSO regressions for/on
@@ -29,36 +39,39 @@ library_list <- c(library(plyr),library(dplyr),library(stringi),
 # are typically called returned for any regression ran using R
 #CL3 <- makeCluster(detectCores() - 1L)
 #clusterExport(CL3, c('datasets'))
+grid <- 10^seq(10, -2, length = 100)
 set.seed(11)     # to ensure replicability
-system.time(L.fits <- lapply(X = datasets, function(i) 
+system.time(glmnet_lasso.fits <- lapply(X = datasets, function(i) 
                glmnet(x = as.matrix(select(i, starts_with("X"))), 
-                      y = i$Y, alpha = 1)))
+                      y = i$Y, alpha = 1, lambda = grid)))
 
+# Use cross-validation to select the penalty parameter
+system.time(cv_glmnet_lasso.fits <- lapply(datasets, function(i) 
+  cv.glmnet(x = as.matrix(select(i, starts_with("X"))), y = i$Y, alpha = 1)))
 
-# This stores and prints out all of the regression 
-# equation specifications selected by LASSO when called
-L.coefs = L.fits |> 
-  Map(f = \(model) coef(model, s = .1))
+# Store and print out the regression equation specifications selected by LASSO when called
+lasso.coeffs = cv_glmnet_lasso.fits |> 
+  Map(f = \(model) coef(model, s = "lambda.min"))
 
-Variables.Selected <- L.coefs |>
+Variables.Selected <- lasso.coeffs |>
   Map(f = \(matr) matr |> as.matrix() |> 
-       as.data.frame() |> filter(s1 != 0) |> rownames())
+        as.data.frame() |> filter(s1 != 0) |> rownames())
 
 Variables.Selected = lapply(seq_along(datasets), \(j)
-                                         j <- (Variables.Selected[[j]][-1]))
+                            j <- (Variables.Selected[[j]][-1]))
 
-Variables.Not.Selected <- L.coefs |>
+Variables.Not.Selected <- lasso.coeffs |>
   Map(f = \(matr) matr |> as.matrix() |> 
         as.data.frame() |> filter(s1 == 0) |> rownames())
 
-write.csv(
-  data.frame(DS_name = DS_names_list, 
+write.csv(data.frame(DS_name = DS_names_list, 
              Variables.Selected.by.glmnet = sapply(Variables.Selected, toString),
              Structural_Variables = sapply(Structural_Variables, 
                                            toString),
+             Variables.Not.Selected.by.glmnet = sapply(Variables.Not.Selected, toString),
              Nonstructural_Variables = sapply(Nonstructural_Variables, 
                                               toString)),
-  file = "LASSO's Selections via glmnet for the DSs from '0-5-1-1 to 0-6-10-500.csv", 
+  file = "LASSO's Selections via glmnet for the DSs from '0.75-9-1-1 to 0.75-9-10-500.csv", 
   row.names = FALSE)
 
 
@@ -170,9 +183,8 @@ performance_metrics <- data.frame(PMs1, PMs2, PMs3)
 performance_metrics
 
 write.csv(performance_metrics, 
-          file = "glmnet's Performance on the datasets from '0-5-1-1 to 0-6-10-500'.csv", 
+          file = "glmnet's Performance on the datasets from '0.75-9-1-1 to 0.75-9-10-500'.csv", 
           row.names = FALSE)
-
 
 length(datasets)
 head(DS_names_list)
@@ -182,6 +194,4 @@ tail(DS_names_list)
 #rm(Variables.Selected, LASSO_fits, LASSO_Coeffs)
 #rm(BM1_FPR, BM1_FPs, BM1_NNs, BM1_NPs, BM1_TNR, BM1_TPR, BM1_TPs)
 #rm(FPR, TPR, TNR, Headers, num_null_FPR)
-
 #rm(performance_metrics, PMs1, PMs2, PMs3)
-
