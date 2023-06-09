9169b65d265f870149718c5f607ea721d50f16c9
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-06-09 14:19:09 -0700

    Add files via upload

--- Stage 2 scripts/LASSO using cv.lars (regression part only).R
+++ Stage 2 scripts/LASSO using cv.lars (regression part only).R
@@ -4,7 +4,7 @@
 rm(list = ls())
 # find out which working directory R has defaulted to
 getwd()
-system.time( load("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER Project/Saved WorkSpaces/Workspaces for dataset folders starting with '0.75'/datasets WorkSpace for '0.75-15-1-1 to 0.75-15-10-500'.RData") )
+system.time( load("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER Project/Saved WorkSpaces/Workspaces for dataset folders starting with '0'/datasets WorkSpace for '0-3-1-1 to 0-4-10-500'.RData") )
 #Structural_Variables <- True_Regressors
 
 # load all necessary packages using only 1 command/line
@@ -12,26 +12,52 @@ library_list <- c(library(dplyr),library(stringi),library(stats),
                   library(data.table),library(leaps),library(lars),
                   library(parallel))
 
+
+
+# assign all 30 candidate regressor names to an object
+var_names <- c("X1","X2","X3","X4","X5","X6","X7","X8",
+               "X9","X10","X11","X12","X13","X14","X15",
+               "X16","X17","X18","X19","X20","X21","X22", 
+               "X23","X24","X25","X26","X27","X28","X29","X30")
+
+
 # This function fits all n LASSO regressions for/on
 # each of the corresponding n datasets stored in the object
 # of that name, then outputs standard regression results which 
 # are typically called returned for any regression ran using R
-set.seed(11)     # to ensure replicability
+#CL <- makeCluster(detectCores() - 4L)
+#system.time( clusterExport(CL, c('datasets')) )
+#set.seed(11)     # to ensure replicability
+#system.time(LASSO.Lars.fits <- parLapply(cl = CL, X = datasets, function(i) 
+#  lars::lars(x = as.matrix(select(i, starts_with("X"))), 
+#       y = i$Y, type = "lasso")))
+#stopCluster(CL)
+#rm(CL)
+
+set.seed(11)  # to ensure replicability
+# Fit the models using cv.lars() instead of lars()
 system.time(LASSO.Lars.fits <- lapply(X = datasets, function(i) 
-  lars(x = as.matrix(select(i, starts_with("X"))), 
-       y = i$Y, type = "lasso", normalize =  FALSE)))
+  cv.lars(x = as.matrix(select(i, starts_with("X"))), 
+          y = i$Y, type = "lasso", normalize =  FALSE, trace = TRUE)))
+print(LASSO.Lars.fits)
 
-# This stores and prints out all of the regression 
-# equation specifications selected by LASSO when called
-set.seed(11)     # to ensure replicability
-system.time(LASSO.Lars.Coeffs <- lapply(LASSO.Lars.fits, 
-                            function(i) predict(i, 
-                                                x = as.matrix(dplyr::select(i, starts_with("X"))), 
-                                                s = 0.1, mode = "fraction", 
-                                                type = "coefficients")[["coefficients"]]))
 
-IVs.Selected.by.Lars <- lapply(LASSO.Lars.Coeffs, function(i) names(i[i > 0]))
-IVs.Not.Selected.by.Lars <- lapply(LASSO.Lars.Coeffs, function(j) names(j[j == 0]))
+LASSO.Lars.Coeffs <- lapply(LASSO.Lars.fits, 
+                            function(i) i$beta[, which.min(i$cv.error)])
+print(LASSO.Lars.Coeffs)
+
+
+
+
+# Extract the names of variables with non-zero coefficients
+IVs.Selected.by.Lars <- lapply(LASSO.Lars.Coeffs, function(i) names(i[i != 0]))
+
+
+
+
+# Retrieve the names of the selected variables
+IVs.Selected.by.Lars <- lapply(optimal_coefficients, function(i) names(i[i != 0]))
+IVs.Not.Selected.by.Lars <- lapply(optimal_coefficients, function(j) names(j[j == 0]))
 
 
 write.csv(data.frame(DS_name = DS_names_list, 
@@ -40,10 +66,10 @@ write.csv(data.frame(DS_name = DS_names_list,
                      Structural_Variables = sapply(Structural_Variables, 
                                                    toString),
                      Unselected_Variables = sapply(IVs.Not.Selected.by.Lars, 
-                                                   toString),
+                                                 toString),
                      NonStructural_Variables = sapply(Nonstructural_Variables, 
-                                                      toString)), 
-          file = "Lars's Selections for the DSs from 0.75-15-1-1 to 0.75-15-10-500.csv", 
+                                                   toString)), 
+          file = "Lars's Selections for the DSs from 0-3-1-1 to 0-4-10-500.csv", 
           row.names = FALSE)
 
 
@@ -56,15 +82,15 @@ write.csv(data.frame(DS_name = DS_names_list,
 Num_of_Positives <- lapply(Structural_Variables, function(i) { length(i) })
 
 True_Positives <- lapply(seq_along(datasets), \(i)
-                         sum(IVs.Selected.by.Lars[[i]] %in% 
-                               Structural_Variables[[i]])) 
+                              sum(IVs.Selected.by.Lars[[i]] %in% 
+                                    Structural_Variables[[i]])) 
 
 Sensitivity = lapply(seq_along(datasets), \(j)
-                     j <- (True_Positives[[j]]/Num_of_Positives[[j]]))
+                  j <- (True_Positives[[j]]/Num_of_Positives[[j]]))
 
 # the number of False Positives & True Negative for each Regression
 Number_of_Negatives <- lapply(Structural_Variables, function(i) 
-{30 - length(i)})
+  {30 - length(i)})
 False_Positives <- lapply(seq_along(datasets), \(i)
                           sum(IVs.Selected.by.Lars[[i]] %in% 
                                 Nonstructural_Variables[[i]])) 
@@ -75,31 +101,31 @@ True_Negatives <- lapply(seq_along(datasets), \(k)
 
 # the number of False Negatives Selected by each LASSO
 False_Negatives <- lapply(seq_along(datasets), \(i)
-                          sum(IVs.Not.Selected.by.Lars[[i]] %in% 
-                                Structural_Variables[[i]]))
+                      sum(IVs.Not.Selected.by.Lars[[i]] %in% 
+                         Structural_Variables[[i]]))
 
 # the False Positive Rate = FP/(FP + TN)
 FalsePosRate = lapply(seq_along(datasets), \(j)
-                      j <- (False_Positives[[j]])/
-                        (False_Positives[[j]] + True_Negatives[[j]]))
+                  j <- (False_Positives[[j]])/
+                    (False_Positives[[j]] + True_Negatives[[j]]))
 
 # the True Negative Rate = TN/(FP + TN)
 Specificity <- lapply(seq_along(datasets), \(j) 
-                      j <- (True_Negatives[[j]]/Number_of_Negatives[[j]]))
+                   j <- (True_Negatives[[j]]/Number_of_Negatives[[j]]))
 
 
 ## calculate the accuracy and F1 score with help from GPT 4
-Accuracy <- lapply(seq_along(datasets), function(i)
-  (True_Positives[[i]] + True_Negatives[[i]])/(True_Positives[[i]] + True_Negatives[[i]] + False_Positives[[i]] + False_Negatives[[i]]))
+BM1_Accuracy <- lapply(seq_along(datasets), function(i)
+  (True_Positives[[i]] + True_Negatives[[i]])/(True_Positives[[i]] + True_Negatives[[i]] + False_Positives[[i]] + BM1_FNs[[i]]))
 
 # First calculate precision and recall for each dataset
-Precision <- lapply(seq_along(datasets), function(i)
+BM1_Precision <- lapply(seq_along(datasets), function(i)
   True_Positives[[i]]/(True_Positives[[i]] + False_Positives[[i]]))
-Recall <- Sensitivity  # You have already calculated recall as True Positive Rate (TPR)
+BM1_Recall <- Sensitivity  # You have already calculated recall as True Positive Rate (TPR)
 
 # Then calculate F1 score for each dataset
-F1_Score <- lapply(seq_along(datasets), function(i)
-  2 * (Precision[[i]] * Recall[[i]])/(Precision[[i]] + Recall[[i]]))
+BM1_F1_Score <- lapply(seq_along(datasets), function(i)
+  2 * (BM1_Precision[[i]] * BM1_Recall[[i]])/(BM1_Precision[[i]] + BM1_Recall[[i]]))
 
 
 ## Write one or more lines of code which determine whether each selected 
@@ -124,7 +150,7 @@ mean_TNR <- round(mean(TNR), 3)
 Under = sum( (TPR < 1) & (FPR == 0) )
 
 # Number of Correctly Specified Regressions Selected by LASSO
-Correct <- sum( (TPR == 1) & (TNR == 1) )
+Correct <- sum( (TPR == 1) & (TNR == 0) )
 
 # number of models with at least one extraneous variable selected
 num_Extraneous <- sum(FPR > 0, na.rm = TRUE)
@@ -135,37 +161,31 @@ Over = sum( (TPR == 1) & (FPR > 0) )
 Num_Under_Correct_or_Over = Under + Correct + Over
 
 
-PMsA <- data.frame(mean(unlist(Accuracy), round = 3), 
-                   mean(unlist(F1_Score), round = 3))
-colnames(PMsA) <- c("Mean Accuracy", "Mean F1 Score")
-
 Headers <- c("True Positive Rate", "True Negative Rate", 
              "False Positive Rate")
-PMsB <- data.frame(mean_TPR, mean_TNR, mean_FPR)
-colnames(PMsB) <- Headers
+PMsA <- data.frame(mean_TPR, mean_TNR, mean_FPR)
+colnames(PMsA) <- Headers
 
 Headers <- c("Underspecified Models Selected", 
              "Correctly Specified Models Selected",
              "Overspecified Models Selected")
-PMsC <- data.frame(Under, Correct, Over)
-colnames(PMsC) <- Headers
+PMsB <- data.frame(Under, Correct, Over)
+colnames(PMsB) <- Headers
 
 Headers <- c("All Correct, Over, and Underspecified Models", 
              "Models with at least one Omitted Variable",
              "Models with at least one Extra Variable")
-PMsD <- data.frame(Num_Under_Correct_or_Over, num_OMVs, num_Extraneous)
-colnames(PMsD) <- Headers
+PMsC <- data.frame(Num_Under_Correct_or_Over, num_OMVs, num_Extraneous)
+colnames(PMsC) <- Headers
 rm(num_OMVs, num_Extraneous)
 
 # Or, just print out this instead of having to print out 3 different things
-Lars_performance <- data.frame(PMsA, PMsB, PMsC, PMsD)
+Lars_performance <- data.frame(PMsA, PMsB, PMsC)
 Lars_performance
 
 write.csv(Lars_performance, 
-          file = "Lars's Performance on the datasets from 0.75-15-1-1 to 0.75-15-10-500.csv", 
+          file = "Lars's Performance on the datasets from 0-3-1-1 to 0-4-10-500.csv", 
           row.names = FALSE)
 
-length(datasets)
-head(DS_names_list)
-tail(DS_names_list)
-getwd()
+#stopCluster(CL)
+#rm(CL)
