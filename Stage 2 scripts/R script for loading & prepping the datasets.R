28ca8f59205774984be881e2080f0f34190ea480
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-06-21 06:18:22 -0700

    Update R script for loading & prepping the datasets.R
    I added a lot more useful commentary for anyone using it.

--- Stage 2 scripts/R script for loading & prepping the datasets.R
+++ Stage 2 scripts/R script for loading & prepping the datasets.R
@@ -4,7 +4,7 @@
 
 ## This script in its entirety can be run by the user by hitting Ctrl+Alt+R.
 rm(list = ls())
-setwd("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project")
+#setwd("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project")
 getwd()
 
 # load all necessary packages
@@ -13,12 +13,17 @@ library(dplyr)
 library(stringi)
 library(purrr)
 library(data.table)
-#library(parallel)
+library(parallel)
 
-# Extract all of the individual spreadsheet containing workbooks
-# in the file folder called 'Data' which is filled
-# random synthetic observations to run FS, Stepwise, and eventually EER
-# on to compare the results. There are 260k spreadsheets in this folder
+
+# Extract all of the individual spreadsheet containing csv files
+# in the file folder called 'Data' which is filled random synthetic observations 
+# to run FS, Stepwise, and possibly EER on so as to enable proper comparison of their results. 
+# There are 260k spreadsheets in this folder though,
+# so only do this if you have a very powerful computer at hand, my new 32 GB of 
+# RAM having laptop was nowhere near up for that task!
+# So, I run this on subsets I copy+pastad from the Data folder into new folders
+# of either 5,000, 10,000, or 15,000 at a time and just named them by the range of datasets they include.
 folderpath <- "C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project/Data Folders/csvs/0.5-12-1-1 to 0.5-12-10-500"
 system.time(paths_list <- list.files(path = folderpath, 
                                      full.names = TRUE,  
@@ -45,11 +50,15 @@ paths_list = paths_list[my_order]
 ## In order to accomplish this, I will be using the readr library in R.
 ## This line reads all of the data in each of the csv files 
 ## using the name of each store in the list we just created.
-system.time( datasets <- lapply(paths_list, fread) )
-#CL <- makeCluster(detectCores() - 2L)
-#clusterExport(CL, c('paths_list'))
-#system.time(datasets <- parLapply(cl = CL, X = paths_list, 
-#                                  fun = data.table::fread))
+#system.time( datasets <- lapply(paths_list, fread) )
+# depending on how many of the datasets you run this on at a time, it can 
+# really take a long time, I was do subsets of 5k & 10k at a time, and to
+# speed that up, when I am not doing anything else on my laptop that is taxing,
+# I parallelize this importation task.
+CL <- makeCluster(detectCores() - 2L)
+clusterExport(CL, c('paths_list'))
+system.time(datasets <- parLapply(cl = CL, X = paths_list, 
+                                  fun = data.table::fread))
 
 # change column names of all the columns in the data.table 'datasets'
 datasets <- lapply(datasets, function(dataset_i) { 
@@ -71,15 +80,17 @@ var_names <- c("X1","X2","X3","X4","X5","X6","X7","X8",
                "X16","X17","X18","X19","X20","X21","X22", 
                "X23","X24","X25","X26","X27","X28","X29","X30")
 
-# truncate & transform the datasets list before running the regressions
+### truncate & transform the datasets list before running the regressions
+# Step 1: remove the first 2 non-numerical rows of each dataset.
 datasets <- lapply(datasets, function(i) {i[-1:-3, ]})
+# Step 2: they are chr rather than numeric, fix that.
 system.time(datasets <- lapply(datasets, \(X) { lapply(X, as.numeric) }))
+# Step 3: convert all of them to data.tables, a faster alternative to data.frames
 datasets <- lapply(datasets, function(i) { as.data.table(i) })
 
-#stopCluster(CL)
-#rm(CL)
+stopCluster(CL)
+rm(CL)
 rm(paths_list, my_order, folderpath)
 
-#save.image("D:/EER/Saved WorkSpaces/Workspaces for dataset folders starting with '0.5'/datasets WorkSpace for 0.5-12-1-1 to 0.5-12-10-500.RData")
-save.image("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project/Saved WorkSpaces/Workspaces for dataset folders starting with '0.5'/loaded WorkSpace for datasets from '0.5-12-1-1 to 0.5-12-10-500'.RData")
-#save.image("D:/EER folder/WorkSpaces/datasets WorkSpace for 0.5-12-1-1 to 0.5-12-10-500 datasets.RData")
+#save.image("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project/Saved WorkSpaces/Workspaces for dataset folders starting with '0.5'/loaded WorkSpace for datasets from '0.5-12-1-1 to 0.5-12-10-500'.RData")
+
