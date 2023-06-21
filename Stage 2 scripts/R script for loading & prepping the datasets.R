29b1e5c9e48b841c0ccac370f6d24e9a45c399e6
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-06-21 05:46:53 -0700

    Add files via upload

--- Stage 2 scripts/R script for loading & prepping the datasets.R
+++ Stage 2 scripts/R script for loading & prepping the datasets.R
@@ -1,13 +1,10 @@
 # Spencer Marlen-Starr's RStudio script of functions, methods, code, and 
-# code comments required for running Benchmarks 2 & 3, namely the 
-# Backward Elimination & Forward Selection versions of Stepwise Regression
-# on some of our random synthetic observations to see how well it 
-# does so we can compare its results with Dr. Davies' EER procedure.
+# code comments required for running loading and preparing a file folder full
+# of csv file formatted sample datasets for Benchmarking at a time.
 
 ## This script in its entirety can be run by the user by hitting Ctrl+Alt+R.
-#rm(list = ls())
-#setwd("C:/Users/Spencer/Documents/EER Project")
-#setwd("D:/EER")
+rm(list = ls())
+setwd("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project")
 getwd()
 
 # load all necessary packages
@@ -16,14 +13,13 @@ library(dplyr)
 library(stringi)
 library(purrr)
 library(data.table)
-library(parallel)
-
+#library(parallel)
 
 # Extract all of the individual spreadsheet containing workbooks
 # in the file folder called 'Data' which is filled
 # random synthetic observations to run FS, Stepwise, and eventually EER
 # on to compare the results. There are 260k spreadsheets in this folder
-folderpath <- "C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER Project/Data Folders/csvs/0.25-7-1-1 to 0.25-7-10-500"
+folderpath <- "C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project/Data Folders/csvs/0.5-12-1-1 to 0.5-12-10-500"
 system.time(paths_list <- list.files(path = folderpath, 
                                      full.names = TRUE,  
                                      recursive = TRUE))
@@ -50,7 +46,7 @@ paths_list = paths_list[my_order]
 ## This line reads all of the data in each of the csv files 
 ## using the name of each store in the list we just created.
 system.time( datasets <- lapply(paths_list, fread) )
-#CL <- makeCluster(detectCores() - 5L)
+#CL <- makeCluster(detectCores() - 2L)
 #clusterExport(CL, c('paths_list'))
 #system.time(datasets <- parLapply(cl = CL, X = paths_list, 
 #                                  fun = data.table::fread))
@@ -67,13 +63,13 @@ Structural_IVs <- lapply(datasets, function(j) {j[1, -1]})
 Structural_Variables <- lapply(Structural_IVs, function(i) {names(i)[i == 1]})
 Nonstructural_Variables <- lapply(Structural_IVs, function(i) {
   names(i)[i == 0] })
+All_Variable_Names <- lapply(datasets, function(k) {names(k)})
 
 # assign all 30 candidate regressor names to an object
 var_names <- c("X1","X2","X3","X4","X5","X6","X7","X8",
                "X9","X10","X11","X12","X13","X14","X15",
                "X16","X17","X18","X19","X20","X21","X22", 
                "X23","X24","X25","X26","X27","X28","X29","X30")
-
 
 # truncate & transform the datasets list before running the regressions
 datasets <- lapply(datasets, function(i) {i[-1:-3, ]})
@@ -82,11 +78,8 @@ datasets <- lapply(datasets, function(i) { as.data.table(i) })
 
 #stopCluster(CL)
 #rm(CL)
+rm(paths_list, my_order, folderpath)
 
-
-#save.image("D:/EER/Saved WorkSpaces/Workspaces for dataset folders starting with '0.25'/datasets WorkSpace for 0.25-7-1-1 to 0.25-7-10-500.RData")
-save.image("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER Project/Saved WorkSpaces/datasets WorkSpace for '0.25-7-1-1 to 0.25-7-10-500'.RData")
-#save.image("D:/EER folder/WorkSpaces/datasets WorkSpace for 0.25-7-1-1 to 0.25-7-10-500 datasets.RData")
-
-
-
+#save.image("D:/EER/Saved WorkSpaces/Workspaces for dataset folders starting with '0.5'/datasets WorkSpace for 0.5-12-1-1 to 0.5-12-10-500.RData")
+save.image("C:/Users/Spencer/OneDrive/Documents/Analytics Projects/EER project/Saved WorkSpaces/Workspaces for dataset folders starting with '0.5'/loaded WorkSpace for datasets from '0.5-12-1-1 to 0.5-12-10-500'.RData")
+#save.image("D:/EER folder/WorkSpaces/datasets WorkSpace for 0.5-12-1-1 to 0.5-12-10-500 datasets.RData")
